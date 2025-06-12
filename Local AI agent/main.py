6b1c5f80d6095888758f5ee8fbff712ecd1742c6
Riddhesh <135740066+404reese@users.noreply.github.com>
2025-06-12 19:40:26 +0530

    minor improvements

--- Local AI agent/main.py
+++ Local AI agent/main.py
@@ -1,5 +1,6 @@
 from langchain_ollama.llms import OllamaLLM
 from langchain_core.prompts import ChatPromptTemplate
+from embed import retriever
 
 model = OllamaLLM(model="llama3.2:3b")
 
@@ -8,14 +9,18 @@ You are an expert in answering questions about a pizza restaurant
 
 Here are some relevant reviews:{reviews}
 
-Here is the question to answer"{question}
+Here is the question to answer:{question}
 """
-
 prompt = ChatPromptTemplate.from_template(template)
 chain = prompt | model
 
 while True:
-    question=input("Ask your question or press q to quit : ")
-    if question=="q":
+    print("\n\n-------------------------------")
+    question = input("Ask your question (q to quit): ")
+    print("\n\n")
+    if question == "q":
         break
-    result = chain.invoke({"reviews":reviews,"question":question})
+    
+    reviews = retriever.invoke(question)
+    result = chain.invoke({"reviews": reviews, "question": question})
+    print(result)
